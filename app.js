efc2b0b8594753d2ba854f3d99052ab013d908c8
balo11044 <balo11044@gmail.com>
2018-11-23 19:05:46 +0700

    sds

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.get(
   })
 );
 // profileMiddleware.checkPermission,
-app.get("/profile", (req, res) => {
+app.get("/profile",{withCredentials: true}, (req, res) => {
   console.log("````````````````````````````````````````` req.user: ");
   console.log(req.user);
   console.log("````````````````````````````````````````````````````");
