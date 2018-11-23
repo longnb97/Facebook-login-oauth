9464a5748de24f978cdc1235f64e56bd056e587d
balo11044 <balo11044@gmail.com>
2018-11-23 17:01:00 +0700

    sds

--- app.js
+++ app.js
@@ -86,7 +86,7 @@ passport.use(
     },
     (accessToken, refreshToken, profile, done) => {
       console.log(profile);
-      User.findById(profile.id).then(userFound => {
+      User.findById({ id: profile.id }).then(userFound => {
         if (!userFound) {
           const newUser = new User();
           newUser.id = profile.id;
